99b5a00f46bf2c2aae1169876b8f0a57308f666d
Sebastian L <sl@momou.ch>
2022-09-16 16:45:22 +0200

    Use busybox wget
    - No need for real wget anymore, newer busybox wget works just fine with https urls
    - Add needed libraries for resolving

--- src/etc/initramfs-tools/hooks/wget.sh
+++ src/etc/initramfs-tools/hooks/wget.sh
@@ -5,28 +5,12 @@ case $1 in
 esac
 . /usr/share/initramfs-tools/hook-functions
 
-copy_exec /usr/bin/wget /usr/bin/real_wget
-
-strace_and_copy_libs_for_url ()
-{
-  LIB_PAT="\".*/lib/.*\""
-  STRACED_LIBS=$(strace /usr/bin/wget --no-iri -q -O - "$1" 2>&1 | grep -o "$LIB_PAT")
-
-  echo "$STRACED_LIBS" | while IFS= read -r line
-    do
-    # Strip the quotation marks
-    line="${line%\"}"
-    line="${line#\"}"
-
-    if [ -f "$line" ]
-    then
-      copy_exec "$line"
-    fi
-  done
-}
-
-strace_and_copy_libs_for_url "https://www.debian.org"
-strace_and_copy_libs_for_url "https://raw.githubusercontent.com/stupidpupil/https-keyscript/master/tests/fixtures/encrypted_keyfile"
-strace_and_copy_libs_for_url "https://mozilla-modern.badssl.com"
+# To find out what library are needed do
+# strace busybox wget https://badssl.com 2>&1 | grep open
+for library in "libnss_dns.*" "libnss_files.so.*" "libresolv.so.*" "ld-linux-*.so.*" "libc.so.*"
+do
+	find /lib/ -name '$library' -type f
+	copy_exec "$library"
+done
 
 copy_exec /etc/ssl/certs/ca-certificates.crt
