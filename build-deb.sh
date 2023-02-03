aceee502ac576a6fe5815f07e1e3b71da7944ab2
Sebastian L <sl@momou.ch>
2023-02-03 16:25:01 +0100

    momou! Release 1.0.10
    - Use curl instead of wget (because of troubles with tls connections with busybox wget)
    - Renamed script from wget_or_ask to fetch_or_ask

--- build-deb.sh
+++ build-deb.sh
@@ -3,11 +3,11 @@
 chmod u=rwx src/etc/initramfs-tools/hooks/*.sh
 chmod u=rwx src/etc/initramfs-tools/scripts/init-premount/*.sh
 chmod u=rwx src/etc/initramfs-tools/scripts/init-bottom/*.sh
-chmod u=rwx src/lib/cryptsetup/scripts/wget_or_ask
+chmod u=rwx src/lib/cryptsetup/scripts/fetch_or_ask
 
 chmod og=rx src/etc/initramfs-tools/hooks/*.sh
 chmod og=rx src/etc/initramfs-tools/scripts/init-premount/*.sh
 chmod og=rx src/etc/initramfs-tools/scripts/init-bottom/*.sh
-chmod og=rx src/lib/cryptsetup/scripts/wget_or_ask
+chmod og=rx src/lib/cryptsetup/scripts/fetch_or_ask
 
 dpkg-deb -b src dist
--- src/etc/initramfs-tools/hooks/curl.sh
+++ src/etc/initramfs-tools/hooks/curl.sh
@@ -6,7 +6,7 @@ esac
 . /usr/share/initramfs-tools/hook-functions
 
 # To find out what library are needed do
-# strace busybox wget https://badssl.com 2>&1 | grep open
+# strace curl https://badssl.com 2>&1 | grep open
 for needed_lib in "libnss_dns*.so*" "libnss_files*.so*" "libresolv*.so*" "ld-linux*.so*" "libc-*.so" "libc.so.*"
 do
 	lib=$(find /lib/ -name "$needed_lib" -type f)
@@ -17,3 +17,5 @@ do
 done
 
 copy_exec /etc/ssl/certs/ca-certificates.crt
+copy_exec /usr/bin/curl
+
