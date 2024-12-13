2ec13b5b64722dfce17b8a217924a67a46460557
Sebastian L. <sl@momou.ch>
2024-12-13 15:15:41 +0100

    Fix using arch dependent libraries only

--- src/etc/initramfs-tools/hooks/curl.sh
+++ src/etc/initramfs-tools/hooks/curl.sh
@@ -7,9 +7,10 @@ esac
 
 # To find out what library are needed do
 # strace curl https://badssl.com 2>&1 | grep open
+ARCH=$(/usr/bin/uname -m)
 for needed_lib in "libnss_dns*.so*" "libnss_files*.so*" "libresolv*.so*" "ld-linux*.so*" "libc-*.so" "libc.so.*"
 do
-	lib=$(find /lib/ -name "$needed_lib" -type f)
+	lib=$(/usr/bin/find "/usr/lib/$ARCH-linux-gnu/" -name "$needed_lib" -type f)
 	if [ -n "$lib" ]
 	then
 		copy_exec "$lib"
